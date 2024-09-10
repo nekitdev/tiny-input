ce9b201fd75a51304f4c597743d7ffa6548a40b3
Nikita Tikhonov <nekitdevofficial@gmail.com>
2024-09-10 11:00:18 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,78 @@
+# `tiny-input`
+
+[![License][License Badge]][License]
+[![Version][Version Badge]][Crate]
+[![Downloads][Downloads Badge]][Crate]
+[![Test][Test Badge]][Actions]
+
+> *Tiny input macros.*
+
+## Installation
+
+### `cargo`
+
+You can add `tiny-input` as a dependency with the following command:
+
+```console
+$ cargo add tiny-input
+```
+
+Or by directly specifying it in the configuration like so:
+
+```toml
+[dependencies]
+tiny-input = "0.1.0"
+```
+
+Alternatively, you can add it directly from the source:
+
+```toml
+[dependencies.tiny-input]
+git = "https://github.com/tiny-input/tiny-input.git"
+```
+
+## Documentation
+
+You can find the documentation [here][Documentation].
+
+## Support
+
+If you need support with the library, you can send an [email][Email].
+
+## Changelog
+
+You can find the changelog [here][Changelog].
+
+## Security Policy
+
+You can find the Security Policy of `tiny-input` [here][Security].
+
+## Contributing
+
+If you are interested in contributing to `tiny-input`, make sure to take a look at the
+[Contributing Guide][Contributing Guide], as well as the [Code of Conduct][Code of Conduct].
+
+## License
+
+`tiny-input` is licensed under the MIT License terms. See [License][License] for details.
+
+[Email]: mailto:support@nekit.dev
+
+[Discord]: https://nekit.dev/chat
+
+[Actions]: https://github.com/nekitdev/tiny-input/actions
+
+[Changelog]: https://github.com/nekitdev/tiny-input/blob/main/CHANGELOG.md
+[Code of Conduct]: https://github.com/nekitdev/tiny-input/blob/main/CODE_OF_CONDUCT.md
+[Contributing Guide]: https://github.com/nekitdev/tiny-input/blob/main/CONTRIBUTING.md
+[Security]: https://github.com/nekitdev/tiny-input/blob/main/SECURITY.md
+
+[License]: https://github.com/nekitdev/tiny-input/blob/main/LICENSE
+
+[Crate]: https://crates.io/crates/tiny-input
+[Documentation]: https://docs.rs/tiny-input
+
+[License Badge]: https://img.shields.io/crates/l/tiny-input
+[Version Badge]: https://img.shields.io/crates/v/tiny-input
+[Downloads Badge]: https://img.shields.io/crates/dr/tiny-input
+[Test Badge]: https://github.com/nekitdev/tiny-input/workflows/test/badge.svg
---
+++ src/lib.rs
@@ -0,0 +1,134 @@
+//! Tiny input macros.
+//!
+//! This crate provides three macros for receiving user input:
+//! [`tiny_input!`], [`input!`] and [`raw_input!`].
+//!
+//! [`raw_input!`] is used for when you just need the string (while handling I/O errors):
+//!
+//! ```no_run
+//! use tiny_input::raw_input;
+//!
+//! let name = raw_input!("What is your name? ").unwrap();
+//!
+//! println!("Hello, {name}!");
+//! ```
+//!
+//! [`tiny_input!`] is useful for when panicking on I/O errors is fine,
+//! and you only need to parse the input:
+//!
+//! ```no_run
+//! use tiny_input::tiny_input;
+//!
+//! let value: u64 = tiny_input!("the square of ").unwrap();
+//!
+//! println!("is {}", value * value);
+//! ```
+//!
+//! [`input!`] is when you need to handle both I/O and parsing errors:
+//!
+//! ```no_run
+//! use tiny_input::{input, Error};
+//!
+//! match input!(type f64, "the inverse of ") {
+//!     Ok(value) => println!("is {}", 1.0 / value),
+//!     Err(error) => match error {
+//!         Error::Fetch(fetch_error) => eprintln!("failed to fetch: {fetch_error}"),
+//!         Error::Parse(parse_error) => eprintln!("failed to parse: {parse_error}"),
+//!     },
+//! }
+//! ```
+//!
+//! As one might have noticed, there are two kinds of [`tiny_input!`] and [`input!`],
+//! one that attempts to infer the type, and one where you can provide the type explicitly.
+
+#![forbid(unsafe_code)]
+#![forbid(missing_docs)]
+
+use thiserror::Error;
+
+/// Represents errors that can occur when processing inputs.
+#[derive(Debug, Error)]
+#[error(transparent)]
+pub enum Error<E> {
+    /// Fetch error. Returned when any I/O errors occur,
+    /// such as when writing to [`stdout`] and flushing it,
+    /// as well as when reading from [`stdin`].
+    ///
+    /// [`stdin`]: std::io::stdin
+    /// [`stdout`]: std::io::stdout
+    Fetch(std::io::Error),
+    /// Parse error, which is contrained to implement the [`Error`] trait.
+    /// Returned when parsing into `T` fails; the [`T::Err`] is wrapped into this variant.
+    ///
+    /// [`T::Err`]: std::str::FromStr::Err
+    /// [`Error`]: std::error::Error
+    Parse(E),
+}
+
+/// The specialized result type to be used in this library.
+pub type Result<T, E> = std::result::Result<T, Error<E>>;
+
+/// The message used for expecting values.
+pub const RAW_INPUT_ERROR: &str = "`raw_input!` returned an error";
+
+/// Invokes [`raw_input!`], panicking on I/O errors before parsing the string.
+#[macro_export]
+macro_rules! tiny_input {
+    (type $type: ty $(, $($token: tt)+)?) => {
+        $crate::raw_input!($($($token)+)?).expect($crate::RAW_INPUT_ERROR).parse::<$type>()
+    };
+    ($($token: tt)*) => {
+        $crate::raw_input!($($token)*).expect($crate::RAW_INPUT_ERROR).parse()
+    };
+}
+
+/// Similar to [`tiny_input!`], except I/O and parse errors are wrapped into [`enum@Error<E>`].
+#[macro_export]
+macro_rules! input {
+    (type $type: ty $(, $($token: tt)+)?) => {
+        $crate::raw_input!($($($token)+)?)
+            .map_err($crate::Error::Fetch)
+            .and_then(|string| string.parse::<$type>().map_err($crate::Error::Parse))
+    };
+    ($($token: tt)*) => {
+        $crate::raw_input!($($token)*)
+            .map_err($crate::Error::Fetch)
+            .and_then(|string| string.parse().map_err($crate::Error::Parse))
+    };
+}
+
+/// Fetches raw inputs, returning the resulting [`String`] and propagating I/O errors.
+#[macro_export]
+macro_rules! raw_input {
+    ($($token: tt)+) => {{
+        use ::std::io::Write;
+
+        let mut stdout = ::std::io::stdout().lock();
+
+        // avoid using `?` operator here
+
+        match write!(stdout, $($token)+) {
+            // we do not really need to know the byte count
+            Ok(_) => match stdout.flush() {
+                Ok(_) => $crate::raw_input!(),
+                Err(error) => Err(error),
+            },
+            Err(error) => Err(error),
+        }
+    }};
+    () => {{
+        use ::std::io::BufRead;
+
+        let mut string = ::std::string::String::new();
+
+        match ::std::io::stdin().lock().read_line(&mut string) {
+            // we do not need the byte count here
+            Ok(_) => {
+                string.pop();  // remove the newline character, if there is one
+
+                Ok(string)
+            },
+            Err(error) => Err(error),
+        }
+    }};
+}
